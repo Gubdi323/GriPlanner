c23f4ff8653a2dad0ddd1f228540215ab418654f
Gubdi323 <tomag526@gmail.com>
2026-05-07 12:08:54 +0300

    Partea de logare

---
+++ Planer zilnic de activitate/Planer zilnic de activitate/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Planer_zilnic_de_activitate;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
