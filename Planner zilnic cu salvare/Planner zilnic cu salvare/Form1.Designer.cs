94281590e3a2c075832bc98030c65be5b37c97d6
Gubdi323 <tomag526@gmail.com>
2026-05-07 12:15:10 +0300

    Partea de inregistrare

---
+++ Planner zilnic cu salvare/Planner zilnic cu salvare/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Planner_zilnic_cu_salvare
+{
+    partial class GabiForm1
+    {
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void GabiForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(330, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(590, 632);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.result_photo_5465412061476099092_x;
+            pictureBox5.Location = new Point(141, 20);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(294, 101);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 1;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.google_symbol_2875404;
+            pictureBox4.Location = new Point(291, 554);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(49, 42);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 21;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.facebook_174848;
+            pictureBox3.Location = new Point(17, 554);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(49, 42);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.lock_8472244;
+            pictureBox2.Location = new Point(90, 356);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(28, 28);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.phone;
+            pictureBox1.Location = new Point(90, 302);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(28, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 10F);
+            label10.ForeColor = SystemColors.ControlDark;
+            label10.Location = new Point(111, 485);
+            label10.Name = "label10";
+            label10.Size = new Size(345, 19);
+            label10.TabIndex = 20;
+            label10.Text = "--------------------------------------------------------";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Times New Roman", 10F);
+            label9.ForeColor = SystemColors.HotTrack;
+            label9.Location = new Point(305, 514);
+            label9.Name = "label9";
+            label9.Size = new Size(53, 19);
+            label9.TabIndex = 19;
+            label9.Text = "Log In";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 10F);
+            label8.ForeColor = SystemColors.ControlDark;
+            label8.Location = new Point(190, 514);
+            label8.Name = "label8";
+            label8.Size = new Size(118, 19);
+            label8.TabIndex = 18;
+            label8.Text = "Ai deja un cont?";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Firebrick;
+            button3.Font = new Font("Segoe UI", 12F);
+            button3.ForeColor = SystemColors.Info;
+            button3.ImageKey = "(none)";
+            button3.Location = new Point(340, 549);
+            button3.Name = "button3";
+            button3.Size = new Size(218, 52);
+            button3.TabIndex = 17;
+            button3.Text = "Continua cu Google";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.MidnightBlue;
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.ForeColor = SystemColors.Info;
+            button2.ImageKey = "(none)";
+            button2.Location = new Point(62, 549);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 52);
+            button2.TabIndex = 16;
+            button2.Text = "Continua cu Facebook";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(124, 405);
+            button1.Name = "button1";
+            button1.Size = new Size(321, 52);
+            button1.TabIndex = 15;
+            button1.Text = "Inregistrare";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.Control;
+            textBox5.Location = new Point(124, 356);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Parola";
+            textBox5.Size = new Size(321, 27);
+            textBox5.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 18F);
+            label6.Location = new Point(195, 134);
+            label6.Name = "label6";
+            label6.Size = new Size(180, 34);
+            label6.TabIndex = 12;
+            label6.Text = "Bine ati venit!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F);
+            label2.Location = new Point(190, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 22);
+            label2.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.Location = new Point(295, 199);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Prenume";
+            textBox2.Size = new Size(150, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Control;
+            textBox4.Location = new Point(124, 302);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Telefon";
+            textBox4.Size = new Size(321, 27);
+            textBox4.TabIndex = 9;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Control;
+            textBox3.Location = new Point(124, 249);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Email";
+            textBox3.Size = new Size(321, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(124, 199);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Nume";
+            textBox1.RightToLeft = RightToLeft.No;
+            textBox1.Size = new Size(150, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(370, 424);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 20);
+            label4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F);
+            label1.Location = new Point(124, 174);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Nume/Prenume";
+            // 
+            // GabiForm1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(1231, 752);
+            Controls.Add(panel1);
+            Name = "GabiForm1";
+            Text = "Partea_Inregistrare";
+            Load += GabiForm1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label4;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label6;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox5;
+        private Label label8;
+        private Button button3;
+        private Label label9;
+        private Label label10;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+    }
+}
